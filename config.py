db4d36b267c850129fb1b06aea0bfe3b9b309da9
evopc2000-sys <evopc2000@gmail.com>
2026-08-02 22:23:23 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-################################## Configuration ##################################
-input_guild_id = "server id"  # The ID of the server that you're copying
-output_guild_id = "server id" # The ID of the server that you're pasting onto
-token = "your_token"  your token. (user)
-clear_server = True # Whether to clear the server you're pasting to or not (To delete old channels, roles, etc)
-windows = True # Whether you're on windows or not (for titling)
-###################################################################################
